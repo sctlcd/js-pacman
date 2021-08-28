4be5956be6ecec6e2af85f51b9b50b7597b6198b
8 <lucinda.souchet@outlook.com>
2021-08-28 21:49:22 +0200

    add condition 'if the ghost is scared and pacman is on it' in moveGhost function

--- assets/js/app.js
+++ assets/js/app.js
@@ -208,21 +208,29 @@ function moveGhost(ghost) {
     let direction = directions[Math.floor(Math.random() * directions.length)]
 
     ghost.timerId = setInterval(function() {
-        //if the next square your ghost is going to go in/meet does not have a ghost and does not have a wall
+        // if the next square your ghost is going to go in/meet does not have a ghost and does not have a wall
         if (!squares[ghost.currentIndex + direction].classList.contains('ghost') &&
             !squares[ghost.currentIndex + direction].classList.contains('wall')) {
-            //remove the ghosts classes
+            // remove the ghosts classes
             squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghost')
                 // squares[ghost.currentIndex].classList.remove('ghost', 'scared-ghost')
-                //move into that space
+                // move into that space
             ghost.currentIndex += direction
             squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
-                //else find a new random direction ot go in
+                // else find a new random direction ot go in
         } else direction = directions[Math.floor(Math.random() * directions.length)]
 
-        //if the ghost is scared
+        // if the ghost is scared
         if (ghost.isScared) {
             squares[ghost.currentIndex].classList.add('scared-ghost')
+        }
+
+        // if the ghost is scared and pacman is on it
+        if (ghost.isScared && squares[ghost.currentIndex].classList.contains('pac-man')) {
+            squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghost')
+            ghost.currentIndex = ghost.startIndex
+            score += 100
+            squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
         }
 
     }, ghost.speed)
